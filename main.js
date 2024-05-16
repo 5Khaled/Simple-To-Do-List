1fc239eeff177f38f9c6bb822358279b51fde847
5Khaled <83138623+5Khaled@users.noreply.github.com>
2024-05-17 00:14:38 +0300

    Add files via upload
    123

--- main.js
+++ main.js
@@ -1,280 +1,256 @@
+const tasksBox = document.querySelector(".tasks-container");
 
-let tasks = document.querySelector(".tasks");
+const taskForm = document.querySelector(".task-form");
+const textField = taskForm.querySelector("[type=text]");
+const addButton = taskForm.querySelector("[type=button]");
 
 
 if(window.localStorage.Tasks !== undefined){
   let tasksObject = JSON.parse(window.localStorage.Tasks);
   for(i = 0; i < tasksObject.length; i++){
-    taskName = tasksObject[i].title;
+    const taskName = tasksObject[i].title;
+    const isChecked = tasksObject[i].checked;
     let task = createTask(taskName);
-    tasks.append(task)
+    if(isChecked === true){
+      task.querySelector("[type = checkbox]").checked = true;
+    }
+    tasksBox.append(task)
   }
 }
-else{
-  CreateEmptyPlaceholder();
+
+window.addEventListener('scroll', function(){
+  if(window.innerWidth > 640){
+    if(this.scrollY > 0){
+      taskForm.classList.add("task-form-scroll");
+    }else{
+      taskForm.classList.remove("task-form-scroll");
+    }
+  }else{
+    if(document.body.scrollHeight === this.scrollY + this.window.innerHeight){
+      taskForm.classList.remove("task-form-scroll");
+    }else{
+      taskForm.classList.add("task-form-scroll");
+    }
+  }
+})
+
+const resizeObserver = new ResizeObserver(entries => {
+  const height = entries[0].target.clientHeight;
+  console.log('Body height changed:', height);
+  if(window.scrollY <= 640){
+    if(height > 680){
+      taskForm.classList.add("task-form-scroll");
+    }else{
+      taskForm.classList.remove("task-form-scroll");
+    }
+  }
+})
+resizeObserver.observe(tasksBox)
+
+// Add Button
+addButton.addEventListener('click', function(){
+  taskName = textField.value.trim();
+  textField.focus();
+  if(textValidation(taskName)){
+    addTask(taskName);
+  }else{
+    console.log("Please type something first!");
+  }
+});
+
+
+// Click Events
+document.addEventListener('click', function(e){
+  const clicked = e.target;
+  // Task Options Drop-down Button
+  if([...clicked.classList].includes("options-btn")){
+    optionsDropDown(clicked);
+  }
+  // Options Buttons
+  else if([...clicked.classList].includes("option")){
+    if([...clicked.classList].includes("edit")){
+      editTask(clicked);
+    }
+    else if([...clicked.classList].includes("delete")){
+      deleteTask(clicked);
+    }
+  }
+  else if([...clicked.classList].includes("options")){
+    return;
+  }
+  // Close Options Drop-down
+  else{
+    [...tasksBox.children].forEach(task => {
+      const dropDown = task.children[4];
+      dropDown.classList.remove("active");
+    });
+  }
+})
+
+// Drop-Down Activation
+function optionsDropDown(optionsButton){
+  [...tasksBox.children].forEach(task => {
+    let dropDown = task.children[4];
+    dropDown.classList.remove("active");
+  });
+  const dropDown = optionsButton.closest(".drop-down");
+  dropDown.classList.add("active");
 }
 
-// Add Element
-let addButton = document.querySelector(".add");
-addButton.addEventListener("click", function(){
+// EDIT Task
+function editTask(editButton){
+  [...tasksBox.children].forEach(task => {
+    task.classList.remove("edit-active");
+  });
+  const task = editButton.closest(".task");
+  const editField = task.querySelector(".edit-field");
+  task.classList.add("edit-active");
+  editField.value = task.id;
+  editField.focus();
+}
+// Confirm EDIT
+function editConfirm(confirmButton){
+  const task = confirmButton.closest(".task");
+  const taskName = task.id;
+  const editField = task.querySelector(".edit-field");
+  const label = task.querySelector("label");
+  let tasksObject = JSON.parse(window.localStorage.Tasks);
+  const objIndex = tasksObject.map(obj => obj.title).indexOf(taskName);
+  const newName = editField.value;
 
-  let taskName = document.querySelector(".input").value;
-  // check if user typed any input
-  if(taskName.length > 0){
-    // Check if "Tasks Object" is available in local storage
-    // If NOT then add the first element
-    let task = createTask(taskName);
-    if(window.localStorage.Tasks === undefined){
-      let tasksObject = JSON.stringify([{"title":taskName}]);
+  if(newName !== taskName){
+    if(!tasksObject.map(key => key.title === newName).includes(true)){
+      tasksObject[objIndex]["title"] = newName;
+      tasksObject = JSON.stringify(tasksObject);
       window.localStorage.Tasks = tasksObject;
-      tasks.append(task);
+      task.id = newName;
+      task.classList.remove("edit-active");
+      label.innerHTML = newName;
+    }else{
+      console.log("Task with the same name already exists!");
+      editField.focus();
     }
-    // If "Tasks Object" is available then just adjust the object and re-add it
-    else{
-      let tasksObject = JSON.parse(window.localStorage.Tasks);
-      if(!tasksObject.map(key => key.title === taskName).includes(true)){
-        tasksObject.push({"title": taskName});
-        tasksObject = JSON.stringify(tasksObject);
-        window.localStorage.Tasks = tasksObject;
-        tasks.append(task);
-      }
-    }
-    if(window.localStorage.Tasks !== undefined && tasks.querySelector(".empty") != null){
-      tasks.querySelector(".empty").remove();
-    }
-    document.querySelector(".input").value = "";
+  }else{
+    task.classList.remove("edit-active");
   }
-  document.querySelector(".input").focus();
-})
+}
+
+// DELETE Task
+function deleteTask(deleteButton){
+  const task = deleteButton.closest(".task");
+  const taskName = task.id;
+  let tasksObject = JSON.parse(window.localStorage.Tasks);
+  const objIndex = tasksObject.map(obj => obj.title).indexOf(taskName);
+
+  delete tasksObject[objIndex];
+
+  tasksObject = Object.values(tasksObject);
+  tasksObject = JSON.stringify(tasksObject);  
+  window.localStorage.Tasks = tasksObject;
+
+  task.remove()
+}
 
 
-// Delete element
-// document.addEventListener("click", function(clicked){
-//   let clickedButton = clicked.target;
-//   if(clickedButton.classList.value.split(" ").includes("delete")){
-//     let task = clickedButton.parentElement.parentElement.parentElement;
-//     let taskTitle =  task.querySelector(".title");
-//     let taskName = taskTitle.innerHTML;
-//     let tasksObject = JSON.parse(window.localStorage.Tasks);
-//     let taskID = tasksObject.map(obj => obj.title).indexOf(taskName);
 
-//     delete tasksObject[taskID];
-//     tasksObject = Object.values(tasksObject);
-//     if(tasksObject.length == 0){
-//       window.localStorage.removeItem("Tasks");
-//       CreateEmptyPlaceholder();
-//     }
-//     else{
-//       tasksObject = JSON.stringify(tasksObject);  
-//       window.localStorage.Tasks = tasksObject;
-//     }
-//     task.remove()
-//   }
-// })
 
-// Edit element
-document.querySelector(".tasks").addEventListener("click", function(clicked){
-  let clickedButton = clicked.target;
-  if(clickedButton.classList.value.split(" ").includes("btn")){
-    let task = clickedButton.parentElement.parentElement.parentElement;
-    
-    let taskTitle =  task.querySelector(".title");
-    let taskEditField =  task.querySelector(".edit-field");
-    let taskSettings =  task.querySelector(".settings");
-    let taskButtonsContainer = taskSettings.querySelector(".buttons-container");
-    let taskSettingsBtn = taskSettings.querySelector(".settings-btn");
-    let taskDlt =  task.querySelector(".delete");
-    let taskName = taskTitle.innerHTML;
+// Add Task (Function)
+function addTask(taskName){
+  const task = createTask(taskName);
+  // Check if "Tasks Object" is available in local storage
+  // If NOT then add the first element
+  if(window.localStorage.Tasks === undefined){
+    let tasksObject = JSON.stringify([{"title":taskName, "checked":false}]);
+    window.localStorage.Tasks = tasksObject;
+    tasksBox.append(task);
+  }
+  // If "Tasks Object" is available then just adjust the object and re-add it
+  else{
     let tasksObject = JSON.parse(window.localStorage.Tasks);
-    let taskID = tasksObject.map(obj => obj.title).indexOf(taskName);
-    if(clickedButton.classList.value.split(" ").includes("settings-btn") && !clickedButton.classList.value.split(" ").includes("active")){
-      taskButtonsContainer.style.display = "grid";
-      taskSettingsBtn.style.rotate = "45deg";
-      // taskSettingsBtn.classList.add("active");
-    }
-    else if(clickedButton.classList.value.split(" ").includes("edit")){
-      taskButtonsContainer.style.position = "static";
-      taskButtonsContainer.style.filter = "none";
-      taskSettingsBtn.style.display = "none";
-      taskTitle.style.display = "none";
-      taskDlt.style.display = "none";
-      taskEditField.style.display = "block";
-      taskEditField.value = taskName;
-      clickedButton.classList.remove("edit");
-      clickedButton.classList.add("confirm");
-      clickedButton.innerHTML = "Confirm";
-      taskEditField.focus();
-    }
-    else if(clickedButton.classList.value.split(" ").includes("confirm")){
-      if(taskEditField.value.length > 0){
-        taskButtonsContainer.style.position = "absolute";
-        taskButtonsContainer.style.filter = "drop-shadow(0px 0px 10px #0000002a)";
-        taskSettingsBtn.style.display = "block";
-        taskTitle.style.display = "block";
-        taskDlt.style.display = "block";
-        taskEditField.style.display = "none";
-        clickedButton.classList.remove("confirm");
-        clickedButton.classList.add("edit");
-        clickedButton.innerHTML = "Edit";
-        
-        let taskEdit = taskEditField.value;
-        tasksObject[taskID] = {"title": taskEdit};
-        tasksObject = JSON.stringify(tasksObject);
-        window.localStorage.Tasks = tasksObject;
-        taskTitle.innerHTML = taskEdit;
-      }
-      else{
-        taskEditField.focus();
-      }
-    }
-    else if(clickedButton.classList.value.split(" ").includes("delete")){
-      delete tasksObject[taskID];
-      tasksObject = Object.values(tasksObject);
-      if(tasksObject.length == 0){
-        window.localStorage.removeItem("Tasks");
-        CreateEmptyPlaceholder();
-      }
-      else{
-        tasksObject = JSON.stringify(tasksObject);  
-        window.localStorage.Tasks = tasksObject;
-      }
-      task.remove()
+    if(!tasksObject.map(key => key.title === taskName).includes(true)){
+      tasksObject.push({"title":taskName, "checked":false});
+      tasksObject = JSON.stringify(tasksObject);
+      window.localStorage.Tasks = tasksObject;
+      tasksBox.append(task);
+      textField.value = "";
+    }else{
+      console.log("Task with the same name already exists!");
     }
   }
-})
+}
 
-
-// Create Empty Placeholder
-function CreateEmptyPlaceholder(){
-  let empty = document.createElement("div");
-  empty.classList.add("empty")
-  empty.append(document.createTextNode("You don't have any tasks available!"));
-  tasks.append(empty);
+// Update Checkbox Status
+function checkBox(checkBox){
+  const taskName = checkBox.closest(".task").id;
+  let tasksObject = JSON.parse(window.localStorage.Tasks);
+  const taskID = tasksObject.map(obj => obj.title).indexOf(taskName);
+  if(checkBox.checked === true){
+    tasksObject[taskID]["checked"] = true;
+  }
+  else{
+    tasksObject[taskID]["checked"] = false;
+  }
+  tasksObject = JSON.stringify(tasksObject);
+  window.localStorage.Tasks = tasksObject;
 }
 
 
-// Create a task Element
-// function createTask(taskName){
-//   let task = document.createElement("div");
-//   task.classList.add("task")
-
-//   let taskTitle = document.createElement("div");
-//   taskTitle.classList.add("title")
-//   taskTitle.value = taskName;
-//   taskTitle.append(document.createTextNode(taskName));
-
-//   let taskEditField = document.createElement("input");
-//   taskEditField.classList.add("edit-field");
-//   taskEditField.style.display = "none";
-
-//   let taskDeleteBtn = document.createElement("div");
-//   taskDeleteBtn.classList.add("delete", "btn")
-//   taskDeleteBtn.innerHTML = "Delete";
-
-//   let taskEditBtn = document.createElement("div");
-//   taskEditBtn.classList.add("edit", "btn")
-//   taskEditBtn.innerHTML = "Edit";
-
-//   task.append(taskTitle);
-//   task.append(taskEditField);
-//   task.append(taskEditBtn);
-//   task.append(taskDeleteBtn);
-//   return task;
-// }
-
-// Create a task Element
+// Create Task Element (Function)
 function createTask(taskName){
-  let task = document.createElement("div");
-  task.classList.add("task")
 
-  let taskTitle = document.createElement("div");
-  taskTitle.classList.add("title")
-  taskTitle.value = taskName;
-  taskTitle.append(document.createTextNode(taskName));
+  const editField = document.createElement("input");
+  editField.classList.add("edit-field");
+  // editField.value = taskName;
 
-  let taskEditField = document.createElement("input");
-  taskEditField.classList.add("edit-field");
-  taskEditField.style.display = "none";
+  const confirmButton = document.createElement("button");
+  confirmButton.classList.add("edit-confirm");
+  confirmButton.append(document.createTextNode("Confirm"));
+  confirmButton.setAttribute("onclick", "editConfirm(this)");
 
-  let settings = document.createElement("div");
-  settings.classList.add("settings");
-  let settingsIcon = document.createElement("div");
-  settingsIcon.classList.add("btn", "settings-btn");
-  settingsIcon.innerHTML ="⚙";
-  settings.append(settingsIcon);
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.id = taskName + "-checkbox";
+  checkBox.setAttribute("onclick", "checkBox(this)");
   
-    let taskEditBtn = document.createElement("div");
-    taskEditBtn.classList.add("btn", "edit")
-    taskEditBtn.innerHTML = "Edit";
+  const span = document.createElement("span");
+  span.append(document.createTextNode(taskName));
 
-  let taskDeleteBtn = document.createElement("div");
-  taskDeleteBtn.classList.add("btn", "delete")
-  taskDeleteBtn.innerHTML = "Delete";
+  const label = document.createElement("label");
+  label.setAttribute("for", taskName + "-checkbox");
+  label.append(span);
+  
+  const icon = document.createElement("img");
+  icon.setAttribute("src", "icons/gear.svg");
 
-  let taskTestBtn = document.createElement("div");
-  taskTestBtn.classList.add("test-btn", "btn")
-  taskTestBtn.innerHTML = "Test";
+  const button = document.createElement("button");
+  button.classList.add("options-btn");
+  button.append(icon);
+  
+  const editOption = document.createElement("div");
+  editOption.append(document.createTextNode("Edit"));
+  editOption.classList.add("edit", "option");
 
-  let ButtonsContainer = document.createElement("div");
-  ButtonsContainer.classList.add("buttons-container")
-  ButtonsContainer.style.display = "none";
-  ButtonsContainer.append(taskEditBtn);
-  // ButtonsContainer.append(taskTestBtn);
-  ButtonsContainer.append(taskDeleteBtn);
+  const deleteOption = document.createElement("div");
+  deleteOption.append(document.createTextNode("Delete"));
+  deleteOption.classList.add("delete", "option");
 
-  task.append(taskTitle);
-  task.append(taskEditField);
-  settings.append(ButtonsContainer);
-  task.append(settings);
-  return task;
+  const options = document.createElement("div");
+  options.classList.add("options");
+  options.append(editOption, deleteOption);
+
+  const dropDown = document.createElement("div");
+  dropDown.classList.add("drop-down");
+  dropDown.append(button, options);
+  
+  const taskContainer = document.createElement("div");
+  taskContainer.classList.add("task");
+  taskContainer.id = taskName;
+  taskContainer.append(editField, confirmButton, checkBox, label, dropDown)
+
+  return taskContainer;
 }
 
 
-
-// Create Newwwwwwwwwwwww task Element
-// function createTask2(taskName){
-//   let task = document.createElement("div");
-//   task.classList.add("test-task")
-
-//   let taskTitle = document.createElement("div");
-//   taskTitle.classList.add("title")
-//   taskTitle.value = taskName;
-//   taskTitle.append(document.createTextNode(taskName));
-
-//   let taskEditField = document.createElement("input");
-//   taskEditField.classList.add("edit-field");
-//   taskEditField.style.display = "none";
-
-//   let settings = document.createElement("div");
-//   settings.classList.add("settings");
-//   let settingsIcon = document.createElement("div");
-//   settingsIcon.classList.add("settings-icon");
-//   settingsIcon.innerHTML ="⚙";
-//   settings.append(settingsIcon);
-  
-//     let taskEditBtn = document.createElement("div");
-//     taskEditBtn.classList.add("edit", "btn")
-//     taskEditBtn.innerHTML = "Edit";
-
-//   let taskDeleteBtn = document.createElement("div");
-//   taskDeleteBtn.classList.add("delete", "btn")
-//   taskDeleteBtn.innerHTML = "Delete";
-
-//   let taskTestBtn = document.createElement("div");
-//   taskTestBtn.classList.add("test-btn", "btn")
-//   taskTestBtn.innerHTML = "Test";
-
-//   let ButtonsContainer = document.createElement("div");
-//   ButtonsContainer.classList.add("buttons-container")
-//   ButtonsContainer.append(taskEditBtn);
-//   ButtonsContainer.append(taskTestBtn);
-//   ButtonsContainer.append(taskDeleteBtn);
-
-//   task.append(taskTitle);
-//   task.append(taskEditField);
-//   settings.append(ButtonsContainer);
-//   task.append(settings);
-//   return task;
-// }
-
-// document.querySelector(".tasks-2").append(createTask2("potato"));
+function textValidation(text){
+  return text.length > 0 && text.length <= 40 ? true : false;
+}
